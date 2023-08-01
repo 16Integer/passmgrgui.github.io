bc2d60c570ad0d2ba4e81848c6f61330c054571f
JustAInternetGuy <135318604+JustAInternetGuy@users.noreply.github.com>
2023-08-01 21:48:39 +1000

    hi

--- contact.php
+++ contact.php
@@ -1,6 +1,21 @@
 <?php
-if($_POST["Message"]) {
-mail("xdd1@tutanota.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: jane@janedoe.com");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    // You can customize the email subject and recipient here
+    $to = "xdd1@tutanota.com";
+    $subject = "New Form Submission";
+    $headers = "From: $email";
+    
+    // Send the email
+    $mailResult = mail($to, $subject, $message, $headers);
+    
+    if ($mailResult) {
+        echo "Thank you for your submission!";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
 }
 ?>
