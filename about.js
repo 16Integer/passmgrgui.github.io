2a5c97c0a1d2920a9797c922b36986d1ac5a4f0a
passmgrgui <135318604+passmgrgui@users.noreply.github.com>
2023-08-27 11:11:15 +1000

    Remove loading animation

--- about.js
+++ about.js
@@ -1,7 +1,3 @@
 $(function () {
   $(".menubar").load("menubar.html");
 });
-// Hide the loading overlay when content is loaded
-$(window).on("load", function () {
-  $(".loading-overlay").fadeOut("slow");
-});
--- contact.js
+++ contact.js
@@ -3,7 +3,3 @@ function menu_bar() {
     $(".menubar").load("menubar.html");
   });
 }
-// Hide the loading overlay when content is loaded
-$(window).on("load", function () {
-  $(".loading-overlay").fadeOut("slow");
-});
