f27912f969e58acbed4bf7073c724e529641536f
passmgrgui <135318604+passmgrgui@users.noreply.github.com>
2023-09-15 20:44:30 +1000

    yoo pigies

--- main.js
+++ main.js
@@ -4,22 +4,3 @@ $(function () {
     $(".main").css("display", "block");
   });
 });
-document.addEventListener("DOMContentLoaded", function () {
-  const downloadButton = document.getElementById("download_button");
-  const userAgent = navigator.userAgent;
-
-  downloadButton.addEventListener("click", function () {
-    let downloadUrl =
-      "https://github.com/passmgrgui/passmgr/raw/binaries/linux/passmgrgui.zip";
-
-    if (userAgent.indexOf("Mac OS X") !== -1) {
-      downloadUrl =
-        "https://github.com/passmgrgui/passmgr/raw/binaries/macos/passmgrgui.zip";
-    } else if (userAgent.indexOf("Windows") !== -1) {
-      downloadUrl =
-        "https://github.com/passmgrgui/passmgr/raw/binaries/windows/passmgrguisetup.exe";
-    }
-
-    window.location.href = downloadUrl;
-  });
-});
