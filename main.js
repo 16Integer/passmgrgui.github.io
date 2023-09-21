410a4a1285cf28ad1ccbf7abe5b98c4191e2035f
passmgrgui <135318604+passmgrgui@users.noreply.github.com>
2023-09-21 16:52:41 +1000

    Seriously, I did heeps.

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-$.get("menubar.html", function (data) {
-  // Append the loaded content to the #menu-container
-  $("body").prepend(data);
-  $(".main").css("display", "block");
-});
+fetch("menubar.html")
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (data) {
+    let tempDiv = document.createElement("div");
+    tempDiv.innerHTML = data;
+
+    let navElement = tempDiv.querySelector("nav");
+
+    if (navElement) {
+      document.body.prepend(navElement);
+      document.querySelector(".main").style.display = "block";
+    }
+  })
+  .catch(function (error) {
+    console.error("Error fetching menubar.html:", error);
+  });
